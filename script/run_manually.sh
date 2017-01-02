f9170e4cab25598e1ad88abdc09b1feca77f6641
Heesang Chae <theand@gmail.com>
2017-01-02 14:13:58 +0900

    sudo keepalive, add dash

--- script/run_manually.sh
+++ script/run_manually.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
 # these package needs admin's password during installation.
 
 brew cask install java
@@ -16,3 +23,4 @@ brew cask install macvim
 brew cask install vagrant
 brew cask install virtualbox
 brew cask install xtrafinder
+brew cask install dash
