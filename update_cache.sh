44dc4447faa307cfa737d23244d1b8408fc08926
Heesang Chae <theand@gmail.com>
2017-07-12 17:07:32 +0900

    clean, install script

--- update_cache.sh
+++ update_cache.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -x
 
 
+rm -f Gemfile.lock
+rm -rf .bundle
 bundle install --without development --path .bundle
-bundle exec librarian-puppet install --path=shared --clean
+
+rm -f Puppetfile.lock
+rm -rf .librarian
+rm -rf .tmp
+rm -rf shared
+rm -rf vendor/puppet/cache
+librarian-puppet install --path=shared --clean
