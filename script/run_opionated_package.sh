2436e7ef93887a16c7befaf8d4033c742b9f9215
Heesang Chae <theand@gmail.com>
2017-07-20 14:13:27 +0900

    add opionated package

---
+++ script/run_opionated_package.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+
+source /opt/boxen/env.sh
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###### Brew update&upgrade
+brew update --force
+brew upgrade
+brew tap homebrew/versions
+brew tap caskroom/fonts
+brew tap caskroom/versions
+brew tap caskroom/drivers
+
+###### these package needs admin's password during installation.
+CASK_SUDO_PACKAGES=(
+  duet
+)
+
+for i in ${CASK_SUDO_PACKAGES[@]};
+do
+    brew cask install "$i";
+done
+
+
+###### Brew Packages with options
+
+
+
+###### Brew Packages
+BREW_PACKAGES=(
+  ansible
+  caddy
+  cloc
+  exif
+  exiftags
+  ffmpeg
+  freetype
+  libxml2
+  libyaml
+  testssl
+  typescript
+  x264
+  xvid
+)
+
+for i in ${BREW_PACKAGES[@]};
+do
+    brew install "$i";
+done
+
+
+####### Cask
+
+CASK_PACKAGES=(
+  beyond-compare
+  day-o
+  edgeview
+  google-photos-backup
+  handbrake
+  haroopad
+  libreoffice
+  logitech-unifying
+  macdown
+  marp
+  mojibar
+  ngrok
+  picasa
+  ransomwhere
+  rescuetime
+  scroll-reverser
+  steam
+  timings
+  vagrant-bar
+  vagrant-manager
+  vmware-fusion
+  wordpresscom
+  xnviewmp
+  xtrafinder
+)
+
+for i in ${CASK_PACKAGES[@]};
+do
+    brew cask install "$i";
+done
+
+
+####### FONTS
+
+
+
+
+###### After installation script
+brew config
+brew list
+brew cask list
+brew cleanup
+
+
+###### Switch to using brew-installed bash as default shell
+
